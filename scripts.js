8087518ccb1f3249f6fa8cb466c53ba605afdb9c
joaovrmoura <joaovrmoura4@gmail.com>
2025-09-02 14:33:52 -0300

    css and teste

--- scripts.js
+++ scripts.js
@@ -1,3 +1,36 @@
 function toggleMobileMenu(){
 	document.getElementById("menu").classList.toggle("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    emailjs.init("rnC3l_6Qzv4cfO5nD"); 
+
+    let form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        let params = {
+            from_name: document.getElementById("name").value,
+            email_id: document.getElementById("email").value,
+            message: document.getElementById("message").value
+        };
+
+        emailjs.send("service_fb2b3ci", "template_c05zyzr", params)
+          .then(function (res) {
+			document.getElementById('successMsg').style.display = 'block';
+			document.getElementById('errorMsg').style.display = 'none';
+			form.reset();
+		})
+		.catch(function (err) {
+			document.getElementById('successMsg').style.display = 'none';
+			document.getElementById('errorMsg').style.display = 'block';
+		});
+
+		setTimeout(() => {
+			document.getElementById('successMsg').style.display = 'none';
+			document.getElementById('errorMsg').style.display = 'none';
+		},10000)
+    });
+});
